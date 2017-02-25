4bb977039aa0eaafdddcc6443b2fc7bb69213914
Deigon Prates <deigon_prates@hotmail.com>
2017-02-25 15:12:40 -0300

    correcoes na modelagem do bd

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -11,7 +11,7 @@ class CadastroController extends \HXPHP\System\Controller{
         true
     );
 
-    $this->auth->redirectCheck(false);
+    $this->auth->redirectCheck(true);
   }
 
   public function indexAction(){
