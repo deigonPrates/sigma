e90c0b2a65097278371b97def2320cc086f98b20
Deigon Prates <deigon_prates@hotmail.com>
2017-02-26 18:18:59 -0300

    implementando a funcao listar atividades

--- app/controllers/SigmaController.php
+++ app/controllers/SigmaController.php
@@ -15,7 +15,13 @@ class SigmaController extends \HXPHP\System\Controller{
       $this->view->setFile('listar');
 
   }
-  public function listarAction($user_id = null){
+  public function listarAtividadeAction($user_id = null){
+    $this->view->setFile('listarAtividades');
+    $this->view->setVars([
+        'activity' => Activity::all()
+    ]);
+  }
+  public function listarUsuariosAction($user_id = null){
     $this->view->setFile('listar');
     $this->view->setVars([
         'users' => User::all()
@@ -38,6 +44,28 @@ class SigmaController extends \HXPHP\System\Controller{
 				$user->status = 1;
 				$user->save(false);
 				$this->view->setVar('users', User::all());
+			}
+		}
+	}
+  public function bloquearAtividadeAction($activity_id = null){
+		if (is_numeric($activity_id)) {
+			$activity = Activity::find_by_id($activity_id);
+			if (!is_null($activity)) {
+				$activity->status = 0;
+				$activity->save(false);
+				$this->view->setVar('activity', Activity::all());
+        $this->view->setFile('listarAtividades');
+			}
+		}
+	}
+	public function desbloquearAtividadeAction($activity_id = null){
+		if (is_numeric($activity_id)) {
+			$activity = Activity::find_by_id($activity_id);
+			if (!is_null($activity)) {
+				$activity->status = 1;
+				$activity->save(false);
+				$this->view->setVar('activity', Activity::all());
+          $this->view->setFile('listarAtividades');
 			}
 		}
 	}
