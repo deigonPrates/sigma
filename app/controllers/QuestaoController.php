e70d43b899375c508c0aee4ef67e42a51579dd02
Deigon Prates <deigon_prates@hotmail.com>
2017-03-11 20:18:28 -0300

    finalizando views do aluno

--- app/controllers/QuestaoController.php
+++ app/controllers/QuestaoController.php
@@ -5,6 +5,7 @@ class QuestaoController extends \HXPHP\System\Controller{
   public function __construct($configs){
     parent::__construct($configs);
 
+    $this->load('Storage\Session');
     $this->load('Services\Auth',
         $configs->auth->after_login,
         $configs->auth->after_logout,
@@ -31,21 +32,31 @@ class QuestaoController extends \HXPHP\System\Controller{
       $this->view->setFile('index');
   }
 
-  public function atividadeAction(){
+  public function definirAtividadeAction(){
     $this->view->setFile('index');
     $this->view->setVars([
       'activity' => Activity::all()
     ]);
   }
 
-  public function validacaoAction(){
+  public function responderAvaliacaoAction($contador = null){
+
+    $this->view->setFile('avaliacao');
+
+    if(is_null($contador)){
+        $contador= 1;
+    }
+
     $post = $this->request->post();
+
     if(!empty($post)){
+      $this->session->set('activity_id', $post['activity_id']);
       $this->view->setVars([
         'activity' => $post,
-        'question' => Question::All()
+        'question' => Question::All(),
+        'contador' => $contador
       ]);
-      $this->view->setFile('avaliacao');
+
     }else{
       $this->load('Helpers\Alert', array(
           'danger',
@@ -54,32 +65,50 @@ class QuestaoController extends \HXPHP\System\Controller{
       ));
     }
   }
-  public function gravarespostaAvaliacaoAction(){
-    $post = $this->request->post();
-
-    $teste = User::teste();
-
-
-      /*
+  public function gravaRespostaAvaliacaoAction($contador = null){
     $this->view->setFile('avaliacao');
-    var_dump($question_id);
-    if(!empty($post) && (!is_null($question_id))){
-      $post = array_merge($post,[
-        'user_id' => $this->auth->getUserId(),
-        'question_id' => $question_id
+
+    $post= $this->request->post();
+
+    $activity = [
+      'activity_id' => $this->session->get('activity_id')
+    ];
+
+    if(is_null($contador)){
+        $contador= 1;
+    }else{
+      $contador++;
+    }
+
+    if(!empty($post)){
+      $this->view->setVars([
+        'activity' => $activity,
+        'question' => Question::All(),
+        'contador' => $contador
       ]);
 
-      var_dump($post);
-      # $respostaAvaliacao = Answers::salvarResposta($post);
+      $alternativa = substr($post['alternative'], 0 , 1);
+      $questao_id = substr($post['alternative'],  2);
 
-      if($respostaAvaliacao->status === false){
-        $this->load('Helpers\Alert', array(
-          'danger',
-          'Por favor, corrija os erros encontrados!',
-          $respostaAvaliacao->errors
-        ));
-      }
-    }*/
+      $post = [
+        'user_id' => $this->auth->getUserId(),
+        'question_id' => $questao_id,
+        'alternative' => $alternativa
+      ];
+
+        $gravacao = Answers::salvarResposta($post);
+
+        if($gravacao->status === false){
+          $this->view->setFile('avaliacao');
+          $this->load('Helpers\Alert', array(
+            'danger',
+            'Por favor, corrija os erros encontrados para efetuar o cadastro!',
+            $gravacao->errors
+          ));
+        }
+        $this->view->setFile('avaliacao');
+    }
+
   }
 
   public function visualizarAtividadeAction($activity_id = null){
@@ -87,6 +116,13 @@ class QuestaoController extends \HXPHP\System\Controller{
     $this->view->setVars([
       'activity' => Activity::all()
     ]);
+  }
+
+  public function destruirSessionAction(){
+    $this->session->clear('activity_id');
+
+    $this->view->setPath('home');
+    $this->view->setFile('index');
   }
 
 
